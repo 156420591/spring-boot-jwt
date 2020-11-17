538505b747da487bc2be597d068c3dd5bfeece66
huwenguang <huwenguang@ebankunion.com>
2020-11-17 10:35:51 +0800

    add springboot security debug, to show all filters

--- src/main/java/com/techgeeknext/SpringBootJwtApplication.java
+++ src/main/java/com/techgeeknext/SpringBootJwtApplication.java
@@ -3,6 +3,7 @@ package com.techgeeknext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringBootJwtApplication {
 
